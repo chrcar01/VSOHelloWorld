b8af360f8fce27413397c9c1370608f09502534c
Chris Carter <chrisjcarter@gmail.com>
2019-11-13 18:18:08 +0000

    messing around

--- Program.cs
+++ Program.cs
@@ -1,18 +1,13 @@
 using System;
-
+using System.Diagnostics;
 namespace workspace
 {
     class Program
     {
         static void Main(string[] args)
         {
-            var customer = new Customer();
-            Console.WriteLine(customer.Name);
+            Debugger.Break();
+            Console.WriteLine("Done...");
         }
-    }
-
-    public class Customer
-    {
-        public string Name { get; } = "Chris";
     }
 }
